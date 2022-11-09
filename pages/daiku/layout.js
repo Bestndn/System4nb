99011ae59b7d5e7e1a139e3a29949c89fc57ab8e
NEW幸福 <109276066+NewSatan@users.noreply.github.com>
2022-11-09 15:42:25 +0700

    layout stop

--- pages/daiku/layout.js
+++ pages/daiku/layout.js
@@ -1,25 +1,35 @@
-import { Outlet, Link } from "react-router-dom";
+import React from 'react'
 
-const Layout = () => {
+export default function layout() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
-    </>
+    <div>layout</div>
   )
-};
+}
 
-export default Layout;
+
+// import { Outlet, Link } from "react-router-dom";
+
+// const Layout = () => {
+//   return (
+//     <>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/blogs">Blogs</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//         </ul>
+//       </nav>
+
+//       <Outlet />
+//     </>
+//   )
+// };
+
+// export default Layout;
+
